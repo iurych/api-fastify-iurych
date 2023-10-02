c2e671a3e630d75ae258059bd411606634c87ca7
iurych <iury.vibe@gmail.com>
2023-10-02 09:45:58 -0300

    feat: install eslint

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,6 @@ import fastify from 'fastify'
 
 const app = fastify()
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log('HTTP server is Running')
-  })
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server is Running')
+})
