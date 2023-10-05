544897c52ee40eefb27c6b19491da0c4909c96ea
iurych <iury.vibe@gmail.com>
2023-10-05 14:19:26 -0300

    feat: create listAllTransactions test

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,14 +1,41 @@
-import { expect, test } from 'vitest'
+import { it, beforeAll, afterAll, describe } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
-test('O usuário pode criar uma nova transação', async () => {
-  await request(app.server)
-    .post('/transactions')
-    .send({
-      title: 'New transaction',
-      amount: 5000,
-      type: 'credit',
-    })
-    .expect(201)
+describe('Transactions routes', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('Should be able to create a new transaction', async () => {
+    await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+      .expect(201)
+  })
+
+  it.only('Should be able to list all transactions', async () => {
+    const mockTransactions = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = mockTransactions.get('Set-Cookie')
+
+    await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+  })
 })
